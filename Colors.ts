6381349853e03eb4984efdd12a57c694ebc1ce95
MarinaMonalisa29 <marinamonalisa29@gmail.com>
2026-05-05 21:45:26 -0300

    Add Colors.ts with color constants

---
+++ Colors.ts
@@ -0,0 +1,12 @@
+export const Colors = {
+  primary: "#1D9E75",
+  primaryDark: "#0F6E56",
+  primaryLight: "#9FE1CB",
+  primaryBg: "#EAF3DE",
+  white: "#FFFFFF",
+  black: "#2C2C2A",
+  gray: "#888780",
+  grayLight: "#F1EFE8",
+  grayBorder: "#D3D1C7",
+  background: "#f7f7f5",
+};
